1a0c9567d752ee5f9155ae2f450af5a53222b3ed
DrAtomic <leo.ward.garcia@gmail.com>
2022-11-25 17:11:35 -0800

    create drone type
    make the function names more context specific

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -11,69 +11,79 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
 
-#define BLINK_GPIO0 GPIO_NUM_5
-#define BLINK_GPIO1 GPIO_NUM_18
-#define BLINK_GPIO2 GPIO_NUM_19
-#define BLINK_GPIO3 GPIO_NUM_21
+#define MAX_MOTOR 4
 
-static TaskHandle_t task1, task2, task3, task4;
+#define MOTOR_GPIO0 GPIO_NUM_5
+#define MOTOR_GPIO1 GPIO_NUM_18
+#define MOTOR_GPIO2 GPIO_NUM_19
+#define MOTOR_GPIO3 GPIO_NUM_21
+
+static TaskHandle_t motor_task;
 
 static const BaseType_t app_cpu = 0;
 
-struct arg_struct {
+typedef struct {
 	gpio_num_t gpio;
-	int delay;
-};
+} motor_t;
 
-void toggle_LEDs(void *parameters)
+typedef struct drone {
+	motor_t motors[MAX_MOTOR];
+} drone_t;
+
+drone_t drone;
+
+void setup()
 {
-	struct arg_struct arg = *(struct arg_struct *)parameters;
+	drone.motors[0].gpio = MOTOR_GPIO0;
+	drone.motors[1].gpio = MOTOR_GPIO1;
+	drone.motors[2].gpio = MOTOR_GPIO2;
+	drone.motors[3].gpio = MOTOR_GPIO3;
+}
+
+static void control_motors(void *parameters)
+{
+	motor_t *motors = (motor_t *)parameters;
 
 	while (1) {
-		gpio_set_level(arg.gpio, 0);
-		vTaskDelay(arg.delay);
-		gpio_set_level(arg.gpio, 1);
-		vTaskDelay(arg.delay);
+		for (size_t i = 0; i < MAX_MOTOR; i++)
+			gpio_set_level(motors[i].gpio, 0);
+		vTaskDelay(100);
+		for (size_t i = 0; i < MAX_MOTOR; i++)
+			gpio_set_level(motors[i].gpio, 1);
+		vTaskDelay(100);
 	}
 }
 
 static void create_tasks()
 {
-	static struct arg_struct arg1, arg2, arg3, arg4;
-
-	arg1.gpio = BLINK_GPIO0;
-	arg1.delay = 100;
-
-	arg2.gpio = BLINK_GPIO1;
-	arg2.delay = 150;
-
-	arg3.gpio = BLINK_GPIO2;
-	arg3.delay = 200;
-
-	arg4.gpio = BLINK_GPIO3;
-	arg4.delay = 50;
-
-	xTaskCreatePinnedToCore(toggle_LEDs, "task1", 1204, (void *)&arg1, 1, &task1, app_cpu);
-	xTaskCreatePinnedToCore(toggle_LEDs, "task2", 1204, (void *)&arg2, 1, &task2, app_cpu);
-	xTaskCreatePinnedToCore(toggle_LEDs, "task3", 1204, (void *)&arg3, 1, &task3, app_cpu);
-	xTaskCreatePinnedToCore(toggle_LEDs, "task4", 1204, (void *)&arg4, 1, &task4, app_cpu);
-
+	xTaskCreatePinnedToCore(control_motors,
+				"Motor Control",
+				1204,
+				(void *)&drone.motors,
+				1,
+				&motor_task,
+				app_cpu);
 }
 
-static void setup(void)
+static void arm_motors(void)
 {
-	gpio_set_direction(BLINK_GPIO0, GPIO_MODE_OUTPUT);
-	gpio_set_direction(BLINK_GPIO1, GPIO_MODE_OUTPUT);
-	gpio_set_direction(BLINK_GPIO2, GPIO_MODE_OUTPUT);
-	gpio_set_direction(BLINK_GPIO3, GPIO_MODE_OUTPUT);
+	esp_err_t ret;
+
+	for (size_t i = 0; i < MAX_MOTOR ; i++) {
+		ret = gpio_set_direction(drone.motors[i].gpio, GPIO_MODE_OUTPUT);
+		if (ret != 0) {
+			fprintf(stderr, "Failed to arm engine %zu, Restarting...", i);
+			fflush(stdout);
+			esp_restart();
+		}
+	}
 }
 
 void app_main(void)
 {
 	setup();
+	arm_motors();
 	create_tasks();
 }
